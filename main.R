2b7622e39487f1388241c137dc1275e4eab86fe1
Chikorita <61445279+chikorita-commits@users.noreply.github.com>
2026-05-04 22:55:02 -0700

    Add beehive plot and ANOVA analysis to main.R
    rebranded to main.R too
    
    Signed-off-by: Chikorita <61445279+chikorita-commits@users.noreply.github.com>

--- main.R
+++ main.R
@@ -15,3 +15,14 @@ experiment158<-read.csv("Experiment 158 - tidyverse-friendly.csv")
 #Paired t-testing with alternative hypothesis true mean difference is not equal to 0#
 #####################################################################################
 t.test(experiment158$comic,experiment158$time,paired=TRUE)
+###########################################################################
+#Plotting the beehive plot of font vs. words per minute (Figure 1 in text)#
+###########################################################################
+experiment158a<-data.frame( wpm=c(experiment158$time,experiment158$comic),font=factor( c(rep(c("time"),26),rep(c("comic"),26))))
+experiment158a |>
+  ggplot(aes(x = font, y = wpm)) +
+  geom_beeswarm(cex = 3, size = 3)
+######################################
+#Analysis of Variance (ANOVA) testing#
+######################################
+summary(aov(wpm~font,data=experiment158a))
